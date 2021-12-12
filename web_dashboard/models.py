3310fbd41c6b0690b796c9e2af030c4faf1c1afe
hermannsw <symsapiens@gmail.com>
2021-12-12 16:56:42 +0900

    modified

--- web_dashboard/models.py
+++ web_dashboard/models.py
@@ -44,12 +44,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(
         'staff status',
         default=False,
-        help_text='Designates whether the user can log into this admin site.',
     )
     is_active = models.BooleanField(
         'active',
         default=True,
-        help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.',
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
